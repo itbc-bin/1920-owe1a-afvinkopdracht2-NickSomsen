eaffb6fbc7abee50631f122bceaa399ad84555e3
NickSomsen <55190156+NickSomsen@users.noreply.github.com>
2019-09-17 19:29:51 +0200

    Add files via upload

--- Script aantal nucleotiden sequentie en %CG.py
+++ Script aantal nucleotiden sequentie en %CG.py
@@ -1,21 +1,18 @@
-#De sequence die je wilt analyseren
-seq = "ATGCGATCGGACCATG"
-#.count voor het tellen van de verschillende soorten nucleotiden
+seq = "ATGCATGC"
+
 aantal_a = seq.count("A")
 aantal_t = seq.count("T")
 aantal_c = seq.count("C")
 aantal_g = seq.count("G")
 aantal_n = seq.count("N")
-# voor het tellen van het totaal aantal nucleotiden
 totaal = aantal_a + aantal_t + aantal_c + aantal_g + aantal_n
-# het percentage CG berekenen van de sequentie
 percentage_cg = (aantal_c + aantal_g) / (totaal) * 100
-# printen van de gegevens
-print (aantal_a)
-print (aantal_t)
-print (aantal_c)
-print (aantal_g)
-print (aantal_n)
-print (totaal)
-print (percentage_cg)
+
+print ("A:",aantal_a)
+print ("T:",aantal_t)
+print ("C:",aantal_c)
+print ("G:",aantal_g)
+print ("N:",aantal_n)
+print ("Totaal:",totaal)
+print ("CG%",percentage_cg)
 
